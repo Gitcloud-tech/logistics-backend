53cf186ad053e167e95ef0385827b2512effa062
Gitcloud-tech <gopald099504@gmail.com>
2026-04-03 13:09:19 +0530

    fix: added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.routes.order_routes import router as order_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(order_router)
